5c4247385ab3706a400c0a1061978259c9f054e5
Abdullah <abdullahmokbl0@gmail.com>
2022-11-03 17:02:39 +0200

    ASs

--- components/Code.js
+++ components/Code.js
@@ -5,7 +5,36 @@ export default function Code() {
   return (
     <div className='flex'>
       <h2>Code the perfect design for each project using Atomize.</h2>
-      <div></div>
+      <div className={styles.black}>
+        <span className={styles.y}>const&nbsp;&nbsp;</span>
+        <span className={styles.b}>PaymentCardBlock&nbsp;&nbsp;</span>
+        <span className={styles.w}>=&nbsp;&nbsp;(&nbsp;&nbsp;)&nbsp;&nbsp;=&gt;&nbsp;&nbsp;&#40;</span>
+        <br></br>
+        <span className={styles.b}>&nbsp;&nbsp;&nbsp;&nbsp;&#60;</span>
+        <span className={styles.y}>Div&nbsp;&nbsp;</span>
+        <span className={styles.o}>bg</span>
+        <span className={styles.w}>="</span>
+        <span className={styles.g}>white</span>
+        <span className={styles.w}>"&nbsp;&nbsp;</span>
+        <span className={styles.o}>shadow</span>
+        <span className={styles.w}>="</span>
+        <span className={styles.g}>2</span>
+        <span className={styles.w}>"&nbsp;&nbsp;</span>
+        <span className={styles.o}>rounded</span>
+        <span className={styles.w}>="</span>
+        <span className={styles.g}>xl</span>
+        <span className={styles.w}>"&nbsp;&nbsp;</span>
+        <span className={styles.o}>m</span>
+        <span className={styles.w}>=&#123;&#123;&nbsp;&nbsp;</span>
+        <span className={styles.y}>b</span>
+        <span className={styles.w}>:&nbsp;&nbsp;</span>
+        <span className={styles.g}>"1rem"&nbsp;&nbsp;</span>
+        <span className={styles.w}>&#125;&#125;&nbsp;&nbsp;</span>
+        <span className={styles.o}>p</span>
+        <span className={styles.w}>="</span>
+        <span className={styles.g}>1.5rem</span>
+        <span className={styles.w}>"&#62;</span>
+      </div>
     </div>
   )
 }
