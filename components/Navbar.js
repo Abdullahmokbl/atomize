7772b3a3f4a5a5a6bdf445abdefa3f8ea2f16e0b
Abdullah <abdullahmokbl0@gmail.com>
2022-11-03 14:46:24 +0200

    ddd

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,10 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Link from 'next/link';
 import styles from './../styles/Navbar.module.css';
 
 export default function Navbar() {
+  const [showdp, setShowdp] = useState(false)
+  console.log(showdp)
   return (
     <nav className={styles.nav}>
       <h2>Atomize</h2>
@@ -11,7 +13,18 @@ export default function Navbar() {
         <li>test1</li>
         <li>test2</li>
       </ul>
-      <Link href='profile'><button>Abdullah</button></Link>
+      <Link href='profile'><button className={styles.button}>Abdullah</button></Link>
+      <div className={`${styles.menu} ${showdp? styles.active:''}`} onClick={() => !showdp ? setShowdp(true): setShowdp(false)}>
+        <div></div>
+        <div></div>
+      </div>
+      <div className={`${styles.dropdown} ${showdp? styles.show: ''}`} >
+          <a href='#feautures' onClick={()=> setShowdp(false)}>features</a>
+          <a>test1</a>
+          <a>test2</a>
+          <Link href='profile' onClick={()=> setShowdp(false)}><button>Abdullah</button></Link>
+      </div>
+      <div className=''></div>
     </nav>
   )
 }
